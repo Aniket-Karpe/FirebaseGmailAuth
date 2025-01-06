e859fc560d6c023b9744508338230d0be0be4c92
AniketGit2023 <aniketkarpe2023@gmail.com>
2025-01-06 23:30:27 +0530

    Developed the code as based in Recyclerview Shows data from the realtime database

--- app/src/main/java/com/aklabs/mytestkotlin/Adapters/NotesAdapters.kt
+++ app/src/main/java/com/aklabs/mytestkotlin/Adapters/NotesAdapters.kt
@@ -1,23 +1,39 @@
-//package com.aklabs.mytestkotlin.Adapters
-//
-//import android.view.ViewGroup
-//import androidx.recyclerview.widget.RecyclerView
-//import com.aklabs.mytestkotlin.Model.AddNotes
-//
-//class NotesAdapters(private val notes:List<AddNotes>):RecyclerView.Adapter<NotImplementedError.NoteadapViewHolder>() {
-//    override fun onCreateViewHolder(
-//        parent: ViewGroup,
-//        viewType: Int
-//    ): NotImplementedError.NoteadapViewHolder {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun onBindViewHolder(holder: NotImplementedError.NoteadapViewHolder, position: Int) {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override fun getItemCount(): Int {
-//        TODO("Not yet implemented")
-//    }
-//    class NoteadapViewHolder(private val binding:Note)
-//}
+package com.aklabs.mytestkotlin.Adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.aklabs.mytestkotlin.Model.AddNotes
+import com.aklabs.mytestkotlin.databinding.ActivityShowAllNotesBinding
+import com.aklabs.mytestkotlin.databinding.NotesItemRvBinding
+
+class NotesAdapters(private val notes: List<AddNotes>) :
+    RecyclerView.Adapter<NotesAdapters.NoteadapViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteadapViewHolder {
+        val binding = NotesItemRvBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NoteadapViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(holder: NoteadapViewHolder, position: Int) {
+       val note=notes[position]
+   holder.bind(note)
+    }
+
+
+    override fun getItemCount(): Int {
+       return notes.size
+    }
+
+    class NoteadapViewHolder(private val binding: NotesItemRvBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(note: AddNotes) {
+  binding.tvshowtitle.text=note.title
+  binding.tvshowdescription.text=note.fulldrscriptions
+        }
+
+    }
+}
+
--- app/src/main/java/com/aklabs/mytestkotlin/ShowAllNotes.kt
+++ app/src/main/java/com/aklabs/mytestkotlin/ShowAllNotes.kt
@@ -2,10 +2,60 @@ package com.aklabs.mytestkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.aklabs.mytestkotlin.Adapters.NotesAdapters
+import com.aklabs.mytestkotlin.Model.AddNotes
+import com.aklabs.mytestkotlin.databinding.ActivityAddNewNotesBinding
+import com.aklabs.mytestkotlin.databinding.ActivityShowAllNotesBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class ShowAllNotes : AppCompatActivity() {
+
+    private val binding: ActivityShowAllNotesBinding by lazy {
+        ActivityShowAllNotesBinding.inflate(layoutInflater)
+    }
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_all_notes)
+        setContentView(binding.root)
+        recyclerView=binding.recyclerView
+        databaseReference = FirebaseDatabase.getInstance().getReference("Notes")
+        auth=FirebaseAuth.getInstance()
+        val currentuser=auth.currentUser
+        currentuser?.let  {
+                user->
+            // val notekey=databaseRefrence.child("users").child(user.uid).child("notes").push().key
+            val notekeyrefrence=databaseReference.child("users").child(user.uid).child("notes")
+            notekeyrefrence.addValueEventListener(object :ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val noteslist= mutableListOf<AddNotes>()
+                    for (notessnapshot in snapshot.children){
+                        val note=notessnapshot.getValue(AddNotes::class.java)
+                         note?.let { noteslist.add(it) }
+                    }
+                    val adapter=NotesAdapters(noteslist)
+                    recyclerView.adapter=adapter
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+
+            })
+
+        }
+
+
     }
 }
